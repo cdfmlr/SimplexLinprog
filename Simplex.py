733b0f20dce4d567c0a2967b756d42eadceafd08
cdfmlr <cdfmlr@163.com>
2020-03-25 11:55:10 +0800

    implement vectorized simplex algorithm

--- Simplex.py
+++ Simplex.py
@@ -1,11 +1,13 @@
 from linprog.problem import LpProblem
 from linprog.simplex_method import SimplexMethod
+from linprog.simplex_vectorized import SimplexVectorized
 
-if __name__ == "__main__":
+
+def simplex_method_tests():
     # 普通标准问题
-    # pb = LpProblem([-5, 5, 13, 0, 0], [[-1, 1, 3, 1, 0], [12, 4, 10, 0, 1]], [20, 90])
-    # s = pb.solve(SimplexMethod, show_tab=True)
-    # print(s)
+    pb = LpProblem([-5, 5, 13, 0, 0], [[-1, 1, 3, 1, 0], [12, 4, 10, 0, 1]], [20, 90])
+    s = pb.solve(SimplexMethod, show_tab=True)
+    print(s)
 
     # 唯一解
     pb = LpProblem([2, 3, 0, 0, 0], [[1, 2, 1, 0, 0], [4, 0, 0, 1, 0], [0, 4, 0, 0, 1]], [8, 16, 12])
@@ -13,191 +15,65 @@ if __name__ == "__main__":
     print(s)
 
     # 无穷多解
-    # pb = LpProblem([2, 4, 0, 0, 0], [[1, 2, 1, 0, 0], [4, 0, 0, 1, 0], [0, 4, 0, 0, 1]], [8, 16, 12])
-    # s = pb.solve(SimplexMethod, show_tab=True)
-    # print(s)
+    pb = LpProblem([2, 4, 0, 0, 0], [[1, 2, 1, 0, 0], [4, 0, 0, 1, 0], [0, 4, 0, 0, 1]], [8, 16, 12])
+    s = pb.solve(SimplexMethod, show_tab=True)
+    print(s)
 
     # 无界解
-    # pb = LpProblem([1, 1, 0, 0], [[-2, 1, 1, 0], [1, -1, 0, 1]], [4, 2])
-    # s = pb.solve(SimplexMethod, show_tab=True)
-    # print(s)
+    pb = LpProblem([1, 1, 0, 0], [[-2, 1, 1, 0], [1, -1, 0, 1]], [4, 2])
+    s = pb.solve(SimplexMethod, show_tab=True)
+    print(s)
 
     # 大 M 法
-    # pb = LpProblem([-2, -3, -1, 0, 0], [[1, 4, 2, -1, 0], [3, 2, 0, 0, -1]], [8, 6])
-    # s = pb.solve(SimplexMethod, show_tab=True, big_m=True)
-    # print(s)
+    pb = LpProblem([-2, -3, -1, 0, 0], [[1, 4, 2, -1, 0], [3, 2, 0, 0, -1]], [8, 6])
+    s = pb.solve(SimplexMethod, show_tab=True, big_m=True)
+    print(s)
 
     # 两阶段法
-    # pb = LpProblem([-2, -3, -1, 0, 0], [[1, 4, 2, -1, 0], [3, 2, 0, 0, -1]], [8, 6])
-    # s = pb.solve(SimplexMethod, show_tab=True, two_step=True)
-    # print(s)
+    pb = LpProblem([-2, -3, -1, 0, 0], [[1, 4, 2, -1, 0], [3, 2, 0, 0, -1]], [8, 6])
+    s = pb.solve(SimplexMethod, show_tab=True, two_step=True)
+    print(s)
 
-# import copy
-#
-# import numpy as np
-#
-#
-# class LpProblem(object):
-#     """
-#     LpProblem 描述一个线性规划问题
-#
-#     :attribute c: 目标函数系数,  <math> c = (c_1, c_2, ..., c_n)^T </math>
-#     :attribute a: 系数矩阵,     <math> a = (a_{ij})_{m \times n} = (p_1, p_2, ..., p_n) </math>
-#     :attribute b: 右端常数,     <math> b = (b_1, b_2, ..., b_m)^T, b > 0 </math>
-#     :attribute base_idx: 基变量的下标集合
-#     """
-#
-#     def __init__(self, c: np.array, a: np.array, b: np.array):
-#         self.c = np.array(c, 'float64')
-#         self.a = np.array(a, 'float64')
-#         self.b = np.array(b, 'float64')
-#
-#         self.base_idx = []
-#
-#         # For SimplexMethod
-#         self.entering_idx = -1
-#         self.leaving_idx = -1
-#         self.theta = []
-#         self.tab = []
-#
-#
-# class LpSolve(object):
-#     """
-#     LpSolve 描述一个线性规划问题的解
-#
-#     :attributes success:     是否得到了最优解
-#     :attributes description: 解的描述
-#     :attributes solve:       最优解
-#     """
-#
-#     def __init__(self, success: bool, description: str, solve: np.array):
-#         self.success = success
-#         self.description = description
-#         self.solve = solve
-#
-#
-# class SimplexMethod(object):
-#     pass
-#
-#
-# def simplex_solve(problem: LpProblem, tab=False) -> LpSolve:
-#     """ simplex_solve 对给定<em>标准型</em>线性规划问题使用<em>单纯形</em>算法进行求解
-#
-#     待解决应符合标准型，即：
-#         <math>
-#             max z = c^T * x
-#             s.t. a*x = b, x >= 0
-#         </math>
-#
-#     单纯形算法参考：https://zh.wikipedia.org/zh-hans/单纯形法
-#
-#     可选参数：
-#         tab=True (default False): 计算单纯形表
-#
-#     :return: 问题的解
-#     """
-#
-#     p = copy.deepcopy(problem)
-#
-#     _current_simplex_tab(p)
-#
-#     p.entering_idx = np.argmax(p.c)  # 入基变量
-#
-#     while p.c[p.entering_idx] > 0:
-#         p.theta = []
-#         for i in range(len(p.b)):
-#             if p.a[i][p.entering_idx] > 0:
-#                 p.theta.append(p.b[i] / p.a[i][p.entering_idx])
-#             else:
-#                 p.theta.append(float("inf"))
-#
-#         p.leaving_idx = np.argmin(np.array(p.theta))  # 出基变量
-#
-#         if p.theta[p.leaving_idx] == float("inf"):  # 出基变量 == inf
-#             return LpSolve(False, "出基变量 == inf", None)
-#
-#         _pivot(p)
-#
-#         _current_simplex_tab(p)
-#
-#         p.entering_idx = np.argmax(p.c)
-#
-#     x = np.zeros(len(p.c))
-#     x[p.base_idx] = p.b
-#     print(simplex_tab_tostring(p, None))
-#     return LpSolve(True, "得到最优解", x)
-#
-#
-# def _pivot(p: LpProblem):
-#     """
-#     对给定问题原址执行<em>转轴操作</em>（基变换）
-#     """
-#
-#     main_element = p.a[p.leaving_idx][p.entering_idx]
-#
-#     p.a[p.leaving_idx] /= main_element
-#     p.b[p.leaving_idx] /= main_element
-#
-#     p.base_idx[p.leaving_idx] = p.entering_idx
-#
-#     for i in range(len(p.b)):
-#         if i != p.leaving_idx and p.a[i][p.entering_idx] != 0:
-#             p.b[i] -= p.a[i][p.entering_idx] * p.b[p.leaving_idx]
-#             p.a[i] -= p.a[i][p.entering_idx] * p.a[p.leaving_idx]
-#
-#     p.c -= p.c[p.entering_idx] * p.a[p.leaving_idx]
-#
-#
-# def _current_simplex_tab(p: LpProblem):
-#     """
-#     计算当前单纯形表
-#     :return: None
-#     """
-#     tab = [
-#         (" ", " ") + tuple(p.c) + ("theta",),
-#     ]
-#     if len(p.tab) > 0:
-#         main_element = '%.2f' % p.tab[-1][p.leaving_idx+1][p.entering_idx+3]
-#         p.tab[-1][p.leaving_idx+1][p.entering_idx+2] = f'[{main_element}]'
-#
-#     for i in range(len(p.b)):
-#         if len(p.tab) > 0 and len(p.theta) > 0:
-#             p.tab[-1][i+1][-1] = p.theta[i]
-#
-#         tab += [
-#             [f'x_{p.base_idx[i]}', p.b[i]] + list(p.a[i]) + [" ", ]
-#         ]
-#
-#     p.tab.append(tab)
-#
-#
-# def simplex_tab_tostring(p: LpProblem, step=None):
-#     s = ''
-#     if step is None:
-#         for step in p.tab:
-#             for row in step:
-#                 for i in row:
-#                     s += '%6.6s\t' % i
-#                 s += '\n'
-#             s += '-' * 16 + '\n'
-#     else:
-#         for row in p.tab[step]:
-#             for i in row:
-#                 s += '%6.6s\t' % i
-#                 s += '\n'
-#             s += '-' * 16 + '\n'
-#     return s
-#
-#
-# if __name__ == "__main__":
-#     pb = LpProblem([-5, 5, 13, 0, 0], [[-1, 1, 3, 1, 0], [12, 4, 10, 0, 1]], [20, 90])
-#     pb.base_idx = [3, 4]
-#     s = simplex_solve(pb)
-#     print(s.success)
-#     print(s.description)
-#     print(s.solve)
-#     """
-#     API Design:
-#     pb = LpProblem([-5, 5, 13, 0, 0], [[-1, 1, 3, 1, 0], [12, 4, 10, 0, 1]], [20, 90])
-#     s = pb.solve(Method, **kwargs)
-#     """
+
+def simplex_vectorized_tests():
+    # 普通标准问题
+    pb = LpProblem([-5, 5, 13, 0, 0], [[-1, 1, 3, 1, 0], [12, 4, 10, 0, 1]], [20, 90])
+    s = pb.solve(SimplexVectorized)
+    print(s)
+
+    # 唯一解
+    pb = LpProblem([2, 3, 0, 0, 0], [[1, 2, 1, 0, 0], [4, 0, 0, 1, 0], [0, 4, 0, 0, 1]], [8, 16, 12])
+    s = pb.solve(SimplexVectorized)
+    print(s)
+
+    # 无穷多解
+    pb = LpProblem([2, 4, 0, 0, 0], [[1, 2, 1, 0, 0], [4, 0, 0, 1, 0], [0, 4, 0, 0, 1]], [8, 16, 12])
+    s = pb.solve(SimplexVectorized)
+    print(s)
+
+    # 无界解
+    pb = LpProblem([1, 1, 0, 0], [[-2, 1, 1, 0], [1, -1, 0, 1]], [4, 2])
+    s = pb.solve(SimplexVectorized)
+    print(s)
+
+    # 大 M 法
+    pb = LpProblem([-2, -3, -1, 0, 0], [[1, 4, 2, -1, 0], [3, 2, 0, 0, -1]], [8, 6])
+    s = pb.solve(SimplexVectorized)
+    print(s)
+
+    # 两阶段法
+    pb = LpProblem([-2, -3, -1, 0, 0], [[1, 4, 2, -1, 0], [3, 2, 0, 0, -1]], [8, 6])
+    s = pb.solve(SimplexVectorized)
+    print(s)
+
+
+def simplex_vectorized_debug():
+    pb = LpProblem([2, 3, 0, 0, 0], [[1, 2, 1, 0, 0], [4, 0, 0, 1, 0], [0, 4, 0, 0, 1]], [8, 16, 12])
+    s = pb.solve(SimplexVectorized)
+    print(s)
+
+
+if __name__ == '__main__':
+    # simplex_method_tests()
+    # simplex_vectorized_debug()
+    simplex_vectorized_tests()
---
+++ linprog/simplex_vectorized.py
@@ -0,0 +1,209 @@
+import copy
+
+import numpy as np
+
+from .problem import LpProblem
+from .solve import LpSolve
+from .solver import LpSolver
+
+
+# TODO: Implement a faster inv() for base_inv
+
+
+class SimplexVectorized(LpSolver):
+    """ 矩阵化的单纯形法
+
+    待解决应符合标准型，即：
+    <math>
+        max z = c^T * x
+        s.t. a*x = b, x >= 0
+    </math>
+
+    单纯形算法参考：https://zh.wikipedia.org/zh-hans/单纯形法
+    """
+    class Problem(LpProblem):
+        def __init__(self, c, a, b):
+            super().__init__(c, a, b)
+            self.base_idx = np.ones(len(b), 'int') * -1
+            self.cc = copy.deepcopy(c)
+
+    def __init__(self, problem: LpProblem):
+        super().__init__(problem)
+        self.problem = self.Problem(problem.c, problem.a, problem.b)
+
+    def find_idt_base(self):
+        """
+        尝试找单位阵作初始基
+
+        :return: True if success False else
+        """
+        base_idx = np.ones(len(self.problem.b), 'int') * -1
+        aT = self.problem.a.T
+        for i in range(len(self.problem.b)):
+            e = np.zeros(len(self.problem.b))
+            e[i] = 1
+            for j in range(len(aT)):
+                if np.all(aT[j] == e):
+                    base_idx[i] = j
+
+        self.problem.base_idx = base_idx
+        return np.all(base_idx >= 0)
+
+    def big_m(self, **kwargs):
+        """
+        用大M法得到初始基
+
+        :param kwargs: show_tab=True (default False): 打印运算过程
+        :return: None
+        """
+        M = ((max(abs(self.problem.c)) + 1) ** 2) * 10 + 10
+
+        for i in range(len(self.problem.base_idx)):
+            if self.problem.base_idx[i] < 0:
+                self.problem.c = np.insert(self.problem.c, len(self.problem.c), np.array([-M]))
+
+                ap = np.zeros(len(self.problem.b))
+                ap[i] = 1
+                self.problem.a = np.c_[self.problem.a, ap]
+
+                self.problem.base_idx[i] = len(self.problem.c) - 1
+
+    def two_step(self, **kwargs):
+        """
+        用两阶段法得到初始基，第一阶段在此计算
+
+        :return: 第一阶段的解
+        """
+        p = copy.deepcopy(self.problem)
+        p.c = np.zeros(len(p.c))
+        for i in range(len(self.problem.base_idx)):
+            if self.problem.base_idx[i] < 0:
+                p.c = np.insert(p.c, len(p.c), np.array([-1]))
+
+                ap = np.zeros(len(p.b))
+                ap[i] = 1
+                p.a = np.c_[p.a, ap]
+
+                self.problem.base_idx[i] = len(p.c) - 1
+        p.base_idx = self.problem.base_idx
+        s1 = _simplex_vectorized_solve(p)
+
+        self.problem.c = copy.deepcopy(self.problem.cc)
+        self.problem.base_idx = p.base_idx
+        self.problem.b = p.b
+        self.problem.a = (p.a.T[0: len(self.problem.c)]).T
+        return s1
+
+    def solve(self, **kwargs) -> LpSolve:
+        """
+        单纯形算法入口
+
+        :param kwargs:
+            two_step=True (default False): 使用两阶段法
+            big_m=True    (default True):  使用大 M 法
+        :return: 问题的解
+        """
+        base_idx = kwargs.get("base_idx", [])
+        if base_idx:    # 用户指定了基
+            self.problem.base_idx = base_idx
+        else:
+            if not self.find_idt_base():    # 没有找到单位阵作初始基，用人工变量法（大M法 / 两阶段法）
+                if kwargs.get("two_step", False):
+                    s1 = self.two_step(**kwargs)
+                    if not s1.success:  # 第一阶段确定无可行解
+                        return s1
+                else:
+                    self.big_m(**kwargs)
+        self.problem.base = np.identity(len(self.problem.b))
+        self.problem.base_inv = np.identity(len(self.problem.b))
+
+        pb = copy.deepcopy(self.problem)
+        return _simplex_vectorized_solve(pb)
+
+
+def _simplex_vectorized_solve(p: SimplexVectorized.Problem) -> LpSolve:
+    """ _simplex_vectorized_solve 对给定了初始基的标准型使用<em>矩阵表示的单纯形法</em>进行求解
+
+    :return: 问题的解
+    """
+    base_idx = p.base_idx
+    n_base_idx = _get_n_base_idx(p, base_idx)
+
+    base_inv = np.linalg.inv(p.a.T[base_idx].T)
+
+    # 初始的检验数计算
+    sigma_n = p.c[n_base_idx] - np.dot(np.dot(p.c[base_idx], base_inv), p.a.T[n_base_idx].T)
+
+    while np.any(sigma_n > 0):
+        # 确定入基变量
+        e = np.argmax(sigma_n)
+        entering_idx = n_base_idx[e]
+
+        base_inv_p_entering = np.dot(base_inv, p.a.T[entering_idx].T)
+
+        if np.all(base_inv_p_entering <= 0):
+            return LpSolve(False, "无界解", None, None)
+
+        # 确定换出变量
+        theta = _get_theta(base_inv, base_inv_p_entering, p)
+
+        l = np.argmin(theta)
+        leaving_idx = base_idx[l]
+
+        # 基变换
+        base_idx[l] = entering_idx
+        n_base_idx[e] = leaving_idx
+
+        base_inv = np.linalg.inv(p.a.T[base_idx].T)
+        # Next Sigma_N
+        sigma_n = p.c[n_base_idx] - np.dot(np.dot(p.c[base_idx], base_inv), p.a.T[n_base_idx].T)
+
+    # 迭代结束，计算解的值
+    # base_inv = np.linalg.inv(p.a.T[base_idx].T)
+    xb = np.dot(base_inv, p.b)
+    x = np.zeros(len(p.c))
+    x[base_idx] = xb
+
+    # 分析解的情况
+    return _analyse_solve(p, x, sigma_n)
+
+
+def _get_theta(base_inv, base_inv_p_entering, p):
+    theta = np.ones(len(p.b)) * np.inf
+    for i in range(len(theta)):
+        if base_inv_p_entering[i] > 0:
+            theta[i] = np.dot(base_inv, p.b)[i] / base_inv_p_entering[i]
+    return theta
+
+
+def _get_n_base_idx(p: SimplexVectorized.Problem, base_idx):
+    """
+    初始化非基
+
+    :param p: SimplexVectorized.Problem
+    :param base_idx: 已确定的基indices
+    :return: 非基indices
+    """
+    n_base_idx = list(range(len(p.c)))
+    for i in base_idx:
+        try:
+            n_base_idx.pop(n_base_idx.index(i))
+        except ValueError:
+            pass
+    return np.array(n_base_idx, 'int')
+
+
+def _analyse_solve(p: SimplexVectorized.Problem, x, last_sigma_n) -> LpSolve:
+    x_real = x[0: len(p.cc)]
+    x_personal = x[len(p.cc):]    # 人工变量
+
+    if np.any(x_personal != 0):
+        return LpSolve(False, "无可行解", None, None)
+
+    z = np.dot(x_real, p.cc)
+
+    for i in last_sigma_n:
+        if abs(i) < 1e-8:  # 非基变量检验数为0
+            return LpSolve(True, "无穷多最优解", x_real, z)
+
+    return LpSolve(True, "唯一最优解", x_real, z)
